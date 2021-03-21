b11e76df06169231c603d922961094f174433ff7
Kazuomi Kashii <kazuomi@kashii.net>
2021-03-21 11:53:56 -0700

    Supports type=2.

--- python/dl.py
+++ python/dl.py
@@ -37,12 +37,31 @@ def get_table1(z0):
         z1[k,j] += c
   return z1
 
+def get_table2(z1):
+  m = z1.shape[0]
+  n = z1.shape[1]
+  z2 = np.zeros([m,n], dtype=np.int)
+  z2[:,:] = z1
+  for i in range(m):
+    for j in range(n):
+      c = z1[i,j]
+      if c == 0:
+        continue
+      for k in range(j-1,-1,-1):
+        if z2[i,k] == 0 and i > 0:
+          break
+        z2[i,k] += c
+  return z2
+
 def main():
   z0 = get_table()
   print_table(z0, type=0, d_max=19, l_max=14)
   print('=' * (4+14*9))
   z1 = get_table1(z0)
   print_table(z1, type=1, d_max=19, l_max=14)
+  print('=' * (4+14*9))
+  z2 = get_table2(z1)
+  print_table(z2, type=2, d_max=19, l_max=14)
 
 if __name__ == '__main__':
   main()
