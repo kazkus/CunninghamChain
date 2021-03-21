93ba5c958190fcec9a156f20e0de85e98c589f28
Kazuomi Kashii <kazuomi@kashii.net>
2021-03-21 09:41:58 -0700

    Prints 3 types of tables.

--- python/dl_print.py
+++ python/dl_print.py
@@ -6,27 +6,40 @@ import numpy as np
 def get_dl_df():
   return pd.read_csv('data/dl.csv')
 
-def get_table(df):
-  z = np.zeros([9,9], dtype=np.int)
+def get_tables(df):
+  z0 = np.zeros([9,9], dtype=np.int)
+  z1 = np.zeros([9,9], dtype=np.int)
+  z2 = np.zeros([9,9], dtype=np.int)
   for d,l,c in zip(df['d'], df['l'], df['c']):
-    z[d-1,l-1] = c
-  return z
+    z0[d-1,l-1] = c
+    for i in range(d-1,9):
+      z1[i,l-1] += c
+      for j in range(l):
+        z2[i,j] += c
+  return (z0,z1,z2)
 #
-def print_table(z):
-  print(' \\', end='')
+def print_table(z, type=0):
+  d_column_len = 3
+  print(' ' * (d_column_len-1) + '\\', end='')
   for j in range(9):
     print('|', end='')
-    print('%8d' % (j+1), end='')
+    if type == 2:
+      print('%7d-' % (j+1), end='')
+    else:
+      print('%8d' % (j+1), end='')
   print()
   #
-  print('-' * 2,end='')
+  print('-' * d_column_len,end='')
   for j in range(9):
     print('+', end='')
     print('-' * 8, end='')
   print()
   #
   for i in range(9):
-    print('%2d' % (i+1), end='')
+    if type == 0:
+      print('%3d' % (i+1), end='')
+    else:
+      print('1-%1d' % (i+1), end='')
     for j  in range(9):
         print('|', end='')
         c = z[i,j]
@@ -38,8 +51,12 @@ def print_table(z):
 
 def main():
   df = get_dl_df()
-  z = get_table(df)
-  print_table(z)
+  z0,z1,z2 = get_tables(df)
+  print_table(z0)
+  print("=" * (3+9*9))
+  print_table(z1, type=1)
+  print("=" * (3+9*9))
+  print_table(z2, type=2)
 
 if __name__ == '__main__':
   main()
