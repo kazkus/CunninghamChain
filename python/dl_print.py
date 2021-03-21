43919efbc804772716ff6a94159a45fbdf218cbf
Kazuomi Kashii <kazuomi@kashii.net>
2021-03-21 09:21:59 -0700

    Separates to functions.

--- python/dl_print.py
+++ python/dl_print.py
@@ -3,25 +3,29 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('data/dl.csv')
-#
-z = np.zeros([9,9], dtype=np.int)
-for d,l,c in zip(df['d'], df['l'], df['c']):
+def get_dl_df():
+  return pd.read_csv('data/dl.csv')
+
+def get_table(df):
+  z = np.zeros([9,9], dtype=np.int)
+  for d,l,c in zip(df['d'], df['l'], df['c']):
     z[d-1,l-1] = c
+  return z
 #
-print(' \\', end='')
-for j in range(9):
+def print_table(z):
+  print(' \\', end='')
+  for j in range(9):
     print('|', end='')
     print('%8d' % (j+1), end='')
-print()
-#
-print('-' * 2,end='')
-for j in range(9):
+  print()
+  #
+  print('-' * 2,end='')
+  for j in range(9):
     print('+', end='')
     print('-' * 8, end='')
-print()
-#
-for i in range(9):
+  print()
+  #
+  for i in range(9):
     print('%2d' % (i+1), end='')
     for j  in range(9):
         print('|', end='')
@@ -31,4 +35,12 @@ for i in range(9):
           s = '%8d' % c
         print(s, end='')
     print()
+
+def main():
+  df = get_dl_df()
+  z = get_table(df)
+  print_table(z)
+
+if __name__ == '__main__':
+  main()
 
