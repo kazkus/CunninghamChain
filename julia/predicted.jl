b1047b3ab4c7e74e9fb9e0ac5a99d586dd8b491b
Kazuomi Kashii <kazuomi@kashii.net>
2021-03-28 14:15:09 -0700

    Writes predicted counts to dl0.csv.

--- julia/predicted.jl
+++ julia/predicted.jl
@@ -26,20 +26,31 @@ function predicted_integral(N,k)
     return Int128(round(B[k]*integral)), B[k]*err
 end
 
-function print_predicted_integral(d, k1,k2)
+function print_predicted_integral(d, k1,k2; file=nothing)
   N = Int128(10)
   N = N^d
   println("predicted integral up to 10^$(d)=$(N)")
   for k in k1:k2
     p, err = predicted_integral(N, k)
+    if p == 0
+      continue
+    end
     println("k=$(k) : $(p) ($(err))")
+    if file != nothing
+      s = @sprintf("%d,%d,%d\n", d,k,p)
+      write(file, s)
+    end
   end
 end
 
 function julia_main()
+    f = open("data/dl0.csv", "w")
+    write(f, "d,l,c\n")
     for d in 1:30
-        print_predicted_integral(d, 1,20)
+        # print_predicted_integral(d, 1,20)
+        print_predicted_integral(d, 1,20, file=f)
     end
+    close(f)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
