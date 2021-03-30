4487fcfb79dd30ef194d42b2e3f8a00b1b2c73d1
Kazuomi Kashii <kazuomi@kashii.net>
2021-03-30 00:23:46 -0700

    Tests 2nd kind.

--- julia/primorial_test.jl
+++ julia/primorial_test.jl
@@ -49,18 +49,36 @@ function s_factor_cc(n; kind=1, space=false)
     return s
 end
 
-function print_primorial(n)
-    s1 = s_factor_cc_primorial(n)
-    s2 = s_factor_cc(n)
-    println("$(n) ($(s1)) ($(s2)) $(ndigits(n)) $(CunninghamChains.cunningham_chain(n,kind=1,check=false))")
+function print_primorial(n; kind=1)
+    s1 = s_factor_cc_primorial(n,kind=kind)
+    s2 = s_factor_cc(n,kind=kind)
+    println("$(n) ($(s1)) ($(s2)) $(ndigits(n)) $(CunninghamChains.cunningham_chain(n,kind=kind,check=false))")
 end
 
 Base.@ccallable function julia_main()::Cint
+  println("1st kind")
   print_primorial(91304653283578934559359)
   print_primorial(2759832934171386593519)
   print_primorial(892390227741617675069)
   print_primorial(810433818265726529159)
   print_primorial(90616211958465842219)
+
+  println("2nd kind")
+  print_primorial(2*1540797425367761006138858881-1,kind=2)
+  print_primorial(4*658189097608811942204322721-3,kind=2)
+  print_primorial(1540797425367761006138858881,kind=2)
+  print_primorial(2*658189097608811942204322721-1,kind=2)
+  print_primorial(1284167734361435112645318721,kind=2)
+  print_primorial(2*79910197721667870187016101-1,kind=2)
+  print_primorial(658189097608811942204322721,kind=2)
+  print_primorial(182927793839529342111307801,kind=2)
+  print_primorial(117302256313688977973793781,kind=2)
+  print_primorial(42008163485623434922152331,kind=2)
+  print_primorial(79910197721667870187016101,kind=2)
+  print_primorial(1302312696655394336638441,kind=2)
+  print_primorial(127806074555607670094731,kind=2)
+  print_primorial(40244844789379926979141,kind=2)
+
   return 0
 end
 
