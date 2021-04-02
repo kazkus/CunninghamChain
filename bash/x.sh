6fa5dc7d2981452a6d1cc2e5d046932e4748c4c2
Kazuomi Kashii <kazuomi@kashii.net>
2021-04-02 16:58:50 -0700

    Completed up to 9.0e18, and fixes x.sh.

--- bash/x.sh
+++ bash/x.sh
@@ -4,5 +4,5 @@ D=data/x
 F=$D/x.csv
 
 echo 'l,p' > $F
-cat $D/*.out | grep '^CC' | sed -e 's/ 2021-03-.*//' -e 's/^CC//' -e 's/:/ /' -e 's/ /,/' | sort -t ',' -k2n -u >> $F
+cat $D/*.out | grep '^CC' | sed -e 's/ 2021-0[3-9]-.*//' -e 's/^CC//' -e 's/:/ /' -e 's/ /,/' | sort -t ',' -k2n -u >> $F
 
